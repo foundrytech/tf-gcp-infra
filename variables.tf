de35f510db90950c259e1f7ea416305b1c6a4b29
jicodes <liu.ji1@northeastern.edu>
2024-02-16 16:33:38 -0800

    add configuration files for the terraform project

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "routing_mode" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "subnet_name1" {
+  type = string
+}
+
+variable "ip_cidr_range1" {
+  type = string
+}
+
+
+variable "subnet_name2" {
+  type = string
+}
+
+variable "ip_cidr_range2" {
+  type = string
+}
+
+variable "route_name" {
+  type = string
+}
+
+variable "route_dest_range" {
+  type = string
+}
+
+variable "next_hop_gateway" {
+  type = string
+}
