a6c8dad05f52ac552cfe589e6c9549a6af5441f1
Ji <122512755+jicodes@users.noreply.github.com>
2024-02-23 14:19:09 -0800

    A4 vm (#9)
    * optimize workflow name
    
    * feat: add vm instance and related resources for terraform

--- variables.tf
+++ variables.tf
@@ -42,3 +42,39 @@ variable "route_dest_range" {
 variable "next_hop_gateway" {
   type = string
 }
+
+variable "network_tag" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "image_family" {
+  type = string
+}
+
+variable "image_source_disk" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "disk_type" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
