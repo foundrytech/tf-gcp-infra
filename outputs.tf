75f7886a04d7d46b247a6e18f8fc9699de805a70
jicodes <liu.ji1@northeastern.edu>
2024-02-27 15:51:00 -0800

    update output values after build the instance vm

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
 output "vpc_id" {
   value = google_compute_network.vpc_network.id
 }
+
+output "project_id" {
+  description = "The project id used when managing resources."
+  value       = var.project_id
+}
+
+output "region" {
+  description = "The region used when managing resources."
+  value       = var.region
+}
+
+output "nat_ip" {
+  description = "Public IP address of the example compute instance."
+  value       = google_compute_instance.webapp_instance.network_interface[0].access_config[0].nat_ip
+}
