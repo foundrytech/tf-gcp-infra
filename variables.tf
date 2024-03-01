d67d048c07ecfda1ddcd7a5c63c4c759fcadb076
jicodes <liu.ji1@northeastern.edu>
2024-03-01 08:47:03 -0800

    add new variables for db related sesources

--- variables.tf
+++ variables.tf
@@ -45,7 +45,64 @@ variable "next_hop_gateway" {
 variable "app_tag" {
   type = string
 }
+// [START setup Cloud SQL instance]
+variable "db_version" {
+  type = string
+}
 
+variable "db_edition" {
+  type = string
+}
+
+variable "db_availability_type" {
+  type = string
+}
+
+variable "db_tier" {
+  type = string
+}
+
+variable "db_disk_type" {
+  type = string
+}
+
+variable "db_disk_size" {
+  type = number
+}
+// [END setup Cloud SQL instance]
+
+# [START cloud_sql_postgres_instance_psc_endpoint]
+variable "psc_name" {
+  type = string
+}
+
+variable "psc_address" {
+  type = string
+}
+
+variable "psc_address_type" {
+  type = string
+}
+
+variable "psc_forwarding_rule_name" {
+  type = string
+}
+// [END cloud_sql_postgres_instance_psc_endpoint]
+
+// [START setup db and db user]
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_port" {
+  type = number
+}
+
+// [START setup Compute Engine instance]
 variable "image_family" {
   type = string
 }
