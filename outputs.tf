49ae0bf558460dc342d68bee26f4460e32bd7852
jicodes <liu.ji1@northeastern.edu>
2024-03-02 19:42:39 -0800

    add psc ip to output

--- outputs.tf
+++ outputs.tf
@@ -16,3 +16,8 @@ output "nat_ip" {
   description = "Public IP address of the example compute instance."
   value       = google_compute_instance.app_instance.network_interface[0].access_config[0].nat_ip
 }
+
+output "psc_ip_address" {
+  description = "The private IP address of the postgres instance."
+  value       = google_sql_database_instance.db_instance.private_ip_address
+}
